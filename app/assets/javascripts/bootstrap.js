85985c04623ae3b165b909c37a6037033a0e1b41
Jimmy <jchen@drmcnatty.com>
2013-11-02 22:14:11 -0700

    basic updates

--- app/assets/javascripts/bootstrap.js
+++ app/assets/javascripts/bootstrap.js
@@ -5,11 +5,15 @@ $(document).ready(function($) {
       var hoverItem = $(this).children().text();
       hoverItem = hoverItem.toLowerCase().replace(/\s/g,"");
       $("#"+hoverItem+"_pic").children().css("display","block");
+      console.log("#"+hoverItem+"_pic");
       if ( hoverItem != 'home'){
         $("#home_pic").children().css("display","none");
       }
       else{
         $("#home_pic").children().css("display","block");
+        $("#landscape_pic").children().css("display","none");
+        $("#portrait_pic").children().css("display","none");
+        $("#product_pic").children().css("display","none");
       }
     }
   });
