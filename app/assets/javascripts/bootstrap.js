3bc1a6a573a883d7d28cf9ec2d804000431da748
Jimmy <jchen@drmcnatty.com>
2013-11-02 23:18:05 -0700

    adjust js for hover

--- app/assets/javascripts/bootstrap.js
+++ app/assets/javascripts/bootstrap.js
@@ -1,21 +1,20 @@
 $(document).ready(function($) {
   // hover over top menu buttons. hides home page picture and display each picture
   $(".footer_menu_items").hover(function(){
+    var hoverArr = ["#home_pic", "#landscape_pic", "#portrait_pic", "#product_pic" ] ;
     if (window.location != "/" ){ 
       var hoverItem = $(this).children().text();
       hoverItem = hoverItem.toLowerCase().replace(/\s/g,"");
+      hideOther(hoverArr);
       $("#"+hoverItem+"_pic").children().css("display","block");
-      console.log("#"+hoverItem+"_pic");
-      if ( hoverItem != 'home'){
-        $("#home_pic").children().css("display","none");
-      }
-      else{
-        $("#home_pic").children().css("display","block");
-        $("#landscape_pic").children().css("display","none");
-        $("#portrait_pic").children().css("display","none");
-        $("#product_pic").children().css("display","none");
-      }
     }
   });
 });
+
+function hideOther(otherArr){
+  for(var i = 0 ; i < otherArr.length ; i++){
+    console.log("hi") ;
+    $(otherArr[i]).children().css("display","none");
+  }
+}
 
