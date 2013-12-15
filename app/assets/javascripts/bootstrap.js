cba607a5a04c09e2110f4182e15b764922375949
Jimmy <jchen@drmcnatty.com>
2013-12-15 03:32:57 -0800

    css setting up date

--- app/assets/javascripts/bootstrap.js
+++ app/assets/javascripts/bootstrap.js
@@ -1,6 +1,17 @@
-$(function(){
+$(function () {
   $(".carousel").carousel();
-  $("img").css("min-height", $(window).height());
-  $("img").css("min-width", $(window).width());
-
+  var wHeight = $(window).height();
+  var wWidth = $(window).width();
+  if (wWidth > wHeight) {
+    $("img").css("min-width", wWidth);
+    $("img").css("max-width", wWidth);
+  }
+  else {
+    $("img").css("min-height", wHeight);
+    $("img").css("max-height", wHeight);
+  }
+  $(".carousel-control").css("top", $(window).height() / 2);
+});
+$(window).resize(function(){
+  $(".carousel-control").css("top", $(window).height() / 2);
 });
